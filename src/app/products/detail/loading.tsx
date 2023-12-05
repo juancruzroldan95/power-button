3eb6eeec521e19446cf3af409c61195f038f3e32
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-04 23:51:14 -0300

    Loading UI

---
+++ src/app/products/detail/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function Loading() {
+  return (
+    <div className="w-full h-full min-h-screen flex justify-center items-center">
+      <Image src={'/ercel.svg'} alt="Vercel Logo" width={150} height={150} className="animate-pulse" />
+    </div>
+  );
+}
