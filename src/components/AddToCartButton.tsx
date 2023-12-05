e346b982f865ac12152b5b0638081059eab0a154
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-04 23:51:55 -0300

    Update `<AddToCartButton />` component

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,11 +1,15 @@
 'use client';
 import React from 'react';
 
-export default function AddToCartButton() {
+type AddToCartButtonProps = {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+export default function AddToCartButton({ onClick }: AddToCartButtonProps) {
   return (
     <button
       className="bg-violet-950 hover:bg-violet-400 active:bg-violet-500 text-white font-bold p-2 px-16 my-4 w-full rounded-full border border-solid border-slate-900"
-      onClick={() => alert('Emulando agregar item al carrito')}
+      onClick={onClick}
     >
       Agregar al carrito
     </button>
