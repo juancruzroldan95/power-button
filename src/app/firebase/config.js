670139af0cd3cc1270d576194b23f51a74f0f4b8
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-04 23:48:24 -0300

    config Firebase

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,10 +13,8 @@ const firebaseConfig = {
   storageBucket: 'power-button-80828.appspot.com',
   messagingSenderId: '965081398530',
   appId: '1:965081398530:web:5ca6e629856e8351ebd61b',
-  measurementId: 'G-CY8R2RD63N',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
