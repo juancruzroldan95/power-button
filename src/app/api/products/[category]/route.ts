892eacf466676014f21a4edb53239c3293bd3613
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-05 23:20:25 -0300

    Adding first argument `request` to Dynamic Route Handlers

--- src/app/api/products/[category]/route.ts
+++ src/app/api/products/[category]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "@/app/firebase/config"
 
-export async function GET({ params }: { params: { category: CategoryName } }) { // Get products by category
+export async function GET(request: Request, { params }: { params: { category: CategoryName } }) { // Get products by category
   const { category } = params;
   const querySnapshot = await getDocs(query(collection(db, "products"), where("category", "==", category)));
   const docs = querySnapshot.docs.map(doc => doc.data());
--- src/app/api/products/detail/[slug]/route.ts
+++ src/app/api/products/detail/[slug]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { doc, getDoc, updateDoc } from "firebase/firestore"
 import { db } from "@/app/firebase/config"
 
-export async function GET({ params }: { params: { slug: string } }) {
+export async function GET(request: Request, { params }: { params: { slug: string } }) {
   const { slug } = params;
   const docRef = doc(db, "products", slug);
   const docSnapshot = await getDoc(docRef);
