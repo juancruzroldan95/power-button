43981cc68ded174d79407c7c4940351b7adc6650
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-05 22:50:41 -0300

    Fetchers

--- src/lib/getAllProducts.ts
+++ src/lib/getAllProducts.ts
@@ -1,7 +1,5 @@
-export default async function getAllProducts(category: CategoryName) {
-  const res = await fetch(`${process.env.BASE_URL}/api/products/${category}`, { cache: 'no-store' });
-
+export default async function getAllProducts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/products/`, { cache: 'no-store' });
   if (!res.ok) throw new Error(res.statusText)
-
   return res.json();
 };
--- src/lib/getProduct.ts
+++ src/lib/getProduct.ts
@@ -1,7 +1,5 @@
-export default async function getProduct(slug: string) {
+export default async function getProductDetail(slug: string) {
   const res = await fetch(`${process.env.BASE_URL}/api/products/detail/${slug}`)
-
   if (!res.ok) throw new Error(res.statusText)
-
   return res.json();
 }
