bd640430bd061eae5e10a5caf122938fc426a2a0
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-14 00:48:37 -0300

    Endpoint Delete product by slug

---
+++ src/lib/deleteProduct.ts
@@ -0,0 +1,11 @@
+export default async function updateProduct(slug: string, body: Product) {
+  const res = await fetch(`${process.env.BASE_URL}/api/products/detail/${slug}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+  if (!res.ok) throw new Error(res.statusText)
+  return res.json();
+}
