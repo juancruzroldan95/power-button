5011c24f2b231a21be097d763bcd9a0a9da1b51d
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-22 10:34:25 -0300

    Remove last "/"

--- src/lib/products/getAllProducts.ts
+++ src/lib/products/getAllProducts.ts
@@ -1,5 +1,5 @@
 export default async function getAllProducts() {
-  const res = await fetch(`/api/products/`, { cache: 'no-store' });
+  const res = await fetch(`/api/products`, { cache: 'no-store' });
   if (!res.ok) throw new Error(res.statusText)
   return res.json();
 };
