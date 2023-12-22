85982b884478424b03de29e1bf962527b577c33b
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-22 00:23:47 -0300

    Deploy commit

--- src/lib/products/getAllProducts.ts
+++ src/lib/products/getAllProducts.ts
@@ -1,5 +1,5 @@
 export default async function getAllProducts() {
-  const res = await fetch(`http://localhost:3000/api/products/`, { cache: 'no-store' });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products/`, { cache: 'no-store' });
   if (!res.ok) throw new Error(res.statusText)
   return res.json();
 };
