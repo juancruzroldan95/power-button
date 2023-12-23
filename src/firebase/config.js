8ea5fd0a9fd99628b02f8412ee9eb2c46cb888b0
juancruzroldan95 <juancruz.roldan19@gmail.com>
2023-12-23 11:39:26 -0300

    Errase comments

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyDYC-Fag9h7mJHhQLqr5woOH-QtoxnM-MU',
   authDomain: 'power-button-80828.firebaseapp.com',
@@ -14,7 +12,6 @@ const firebaseConfig = {
   appId: '1:965081398530:web:5ca6e629856e8351ebd61b',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
